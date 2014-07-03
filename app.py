b30e88686e57e8f28b680b4087c4176d3fdd485c
Mattias Hising <hising@gmail.com>
2014-07-03 15:30:53 +0200

    Added latest blog posts to API and home page

--- app.py
+++ app.py
@@ -18,14 +18,26 @@ wordpress = WordpressAPI(api_root = config['cms']['base'])
 
 @app.route("/")
 def home():
+    posts = wordpress.get_recent_posts()
     pages = wordpress.get_pages()
     page = wordpress.get_page('home')
-    return render_template('home.html', pages = pages, page = page)
+    return render_template(
+        'home.html',
+        pages = pages,
+        page = page,
+        posts = posts
+    )
 
 @app.route("/<slug>")
 def get_page(slug):
     page = wordpress.get_page(slug)
     return render_template('page.html', page = page)
+
+
+@app.route("/blog/<slug>")
+def get_blog_post(slug):
+    page = wordpress.get_post(slug)
+    return render_template('post.html', post = post)
 
 if __name__ == "__main__":
     app.run(**config['server'])
--- flaskwp/wordpress.py
+++ flaskwp/wordpress.py
@@ -6,6 +6,14 @@ class WordpressAPI(object):
     def __init__(self, api_root):
         self.api_root = api_root
 
+    def get_recent_posts(self):
+        api_call = "%s/get_recent_posts/" % self.api_root
+        response = requests.get(api_call)
+        if response.status_code == 200:
+            json_response = json.loads(response.text)
+            if (json_response['status'] == 'ok'):
+                return json_response['posts']
+        return []
 
     def get_pages(self):
         api_call = "%s/get_page_index/" % self.api_root
@@ -14,7 +22,7 @@ class WordpressAPI(object):
             json_response = json.loads(response.text)
             if (json_response['status'] == 'ok'):
                 return json_response['pages']
-        return False
+        return []
 
     def get_page(self, slug):
         api_call = "%s/get_page/?slug=%s" % (self.api_root, slug)
@@ -23,4 +31,4 @@ class WordpressAPI(object):
             json_response = json.loads(response.text)
             if (json_response['status'] == 'ok'):
                 return json_response['page']
-        return False
+        return {}
